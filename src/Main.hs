56881437dde9dff7889db1b73d872bf7f7451adb
janmarcano <janmarcano@gmail.com>
2015-05-19 21:36:53 -0430

    'main' listo

--- src/Main.hs
+++ src/Main.hs
@@ -9,12 +9,19 @@ import Diagramas (Orientación(Horizontal, Vertical), caminar, dividir, rectáng
 import Imagen
 
 ciclo :: Ventana -> Diagrama -> [Paso] -> IO ()
-ciclo = undefined
+ciclo v d p = undefined
 
 -- Para tomar los datos del IO de leerImagen
 getImagen :: Either String Imagen -> Imagen
 getImagen (Right (Imagen an al datos)) = Imagen an al datos
 getImagen (Left _) =  Imagen 1 1 [[Color {rojo = 0, verde = 0, azul = 0}]]
+
+getAnchura :: Imagen -> Integer
+getAnchura (Imagen an al d) = an
+getAltura :: Imagen -> Integer
+getAltura (Imagen an al d) = al
+
+pegate x = pegate x
 
 main :: IO ()
 main = do
@@ -23,23 +30,8 @@ main = do
 		(filename:_) -> do
 			results <- (leerImagen filename)
 			let imagen = getImagen results
-			print imagen
-			let h = hSplit imagen
-			let top = hSplit (fst h)
-			print "Color primera fila:"
-			--print colorPromedio (fst top)
-			print "Deberia ser 66, 76, 109"
-			print "Color segunda fila:"
 			let rect = rectánguloImagen imagen
-			let diag = dividir Vertical rect
-			case diag of
-				Just di -> do 
-							let diag2 = (caminar [Primero] di)
-							print "diag1: "
-							print diag
-							print "diag2: "
-							print diag2
-				Nothing -> print "Nothing"
-			--print colorPromedio (snd top)
+			ventana <- (crearVentana (getAnchura imagen) (getAltura imagen))
+			ciclo ventana (Hoja rect) []
 		[] -> do
 			print "No se especifico imagen"
